5cc6c33126a5dc6e5df78a33c297b5844bbb9cab
Miguel <miguelsosa2003@gmail.com>
2023-08-14 10:56:42 -0500

    Tutorial1Activity4

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,10 +8,10 @@ use Illuminate\View\View;
 class ProductController extends Controller
 {
     public static $products = [
-        ["id" => "1", "name" => "TV", "description" => "Best TV"],
-        ["id" => "2", "name" => "iPhone", "description" => "Best iPhone"],
-        ["id" => "3", "name" => "Chromecast", "description" => "Best Chromecast"],
-        ["id" => "4", "name" => "Glasses", "description" => "Best Glasses"]
+        ["id" => "1", "name" => "TV", "description" => "Best TV", "price" => 1000],
+        ["id" => "2", "name" => "iPhone", "description" => "Best iPhone", "price" => 2000],
+        ["id" => "3", "name" => "Chromecast", "description" => "Best Chromecast", "price" => 3000],
+        ["id" => "4", "name" => "Glasses", "description" => "Best Glasses", "price" => 90]
     ];
 
     public function index(): View
