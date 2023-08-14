0afe15f34c43e3212102e4f4cb9895d6a199dcd7
Miguel <miguelsosa2003@gmail.com>
2023-08-14 10:38:13 -0500

    Tutorial1Activity2

---
+++ resources/views/home/contact.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+@section('title', $title)
+@section('subtitle', $subtitle)
+@section('content')
+<div class="text-center">
+    <p>
+        A Normal Store
+    </p>
+    <p>
+    <p>
+        a.normal.store@justnormal.com
+    </p>
+    </p>
+    <p>
+        123456789
+    </p>
+    <p>
+        Latitude: -53.03446, Longitude: 74.72243
+    </p>
+</div>
+@endsection
