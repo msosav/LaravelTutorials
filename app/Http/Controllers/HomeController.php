c2b922a7c4bd66a2082c3ed66263bfecef389fa7
Miguel <miguelsosa2003@gmail.com>
2023-08-16 08:26:56 -0500

    mejoras en el codigo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,26 @@ class HomeController extends Controller
     {
         return view('home.index');
     }
+
+    public function about(): View
+    {
+        $data1 = 'About us - Online Store';
+        $data2 = 'About us';
+        $description = 'This is an about page ...';
+        $author = 'Developed by: Soos';
+
+        return view('home.about')->with('title', $data1)
+            ->with('subtitle', $data2)
+            ->with('description', $description)
+            ->with('author', $author);
+    }
+
+    public function contact(): View
+    {
+        $data1 = 'Contact us - Online Store';
+        $data2 = 'Contact us';
+
+        return view('home.contact')->with('title', $data1)
+            ->with('subtitle', $data2);
+    }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,63 +2,65 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
-use Illuminate\Http\RedirectResponse;
 
 class ProductController extends Controller
 {
     public static $products = [
-        ["id" => "1", "name" => "TV", "description" => "Best TV", "price" => 1000],
-        ["id" => "2", "name" => "iPhone", "description" => "Best iPhone", "price" => 2000],
-        ["id" => "3", "name" => "Chromecast", "description" => "Best Chromecast", "price" => 3000],
-        ["id" => "4", "name" => "Glasses", "description" => "Best Glasses", "price" => 90]
+        ['id' => '1', 'name' => 'TV', 'description' => 'Best TV', 'price' => 1000],
+        ['id' => '2', 'name' => 'iPhone', 'description' => 'Best iPhone', 'price' => 2000],
+        ['id' => '3', 'name' => 'Chromecast', 'description' => 'Best Chromecast', 'price' => 3000],
+        ['id' => '4', 'name' => 'Glasses', 'description' => 'Best Glasses', 'price' => 90],
     ];
 
     public function index(): View
     {
         $viewData = [];
-        $viewData["title"] = "Products - Online Store";
-        $viewData["subtitle"] =  "List of products";
-        $viewData["products"] = ProductController::$products;
-        return view('product.index')->with("viewData", $viewData);
+        $viewData['title'] = 'Products - Online Store';
+        $viewData['subtitle'] = 'List of products';
+        $viewData['products'] = ProductController::$products;
+
+        return view('product.index')->with('viewData', $viewData);
     }
 
-    public function show(string $id): View | RedirectResponse
+    public function show(string $id): View|RedirectResponse
     {
         if (isset(ProductController::$products[$id - 1])) {
             $viewData = [];
             $product = ProductController::$products[$id - 1];
-            $viewData["title"] = $product["name"] . " - Online Store";
-            $viewData["subtitle"] =  $product["name"] . " - Product information";
-            $viewData["product"] = $product;
-            return view('product.show')->with("viewData", $viewData);
+            $viewData['title'] = $product['name'].' - Online Store';
+            $viewData['subtitle'] = $product['name'].' - Product information';
+            $viewData['product'] = $product;
+
+            return view('product.show')->with('viewData', $viewData);
         } else {
-            return redirect()->route("home.index");
+            return redirect()->route('home.index');
         }
     }
 
     public function create(): View
     {
         $viewData = []; //to be sent to the view
-        $viewData["title"] = "Create product";
+        $viewData['title'] = 'Create product';
 
-        return view('product.create')->with("viewData", $viewData);
+        return view('product.create')->with('viewData', $viewData);
     }
 
     public function save(Request $request)
     {
         $viewData = []; //to be sent to the view
-        $viewData["title"] = "Product created";
-        $viewData["subtitle"] = "Product created successfully";
-        $viewData["name"] = $request->input("name");
-        $viewData["price"] = $request->input("price");
+        $viewData['title'] = 'Product created';
+        $viewData['subtitle'] = 'Product created successfully';
+        $viewData['name'] = $request->input('name');
+        $viewData['price'] = $request->input('price');
         $request->validate([
-            "name" => "required",
-            "price" => "required|numeric|gt:0",
+            'name' => 'required',
+            'price' => 'required|numeric|gt:0',
         ]);
 
-        return view('product.saved')->with("viewData", $viewData);
+        return view('product.saved')->with('viewData', $viewData);
         dd($request->all());
         //here will be the code to call the model and save it to the database
     }
